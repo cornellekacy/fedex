66c554201204d767cd37d53a297d56c07102ab8f
cornellekacy <cornellekacy4@gmail.com>
2019-12-09 11:51:56 +0400

    6

--- backend/mail.php
+++ backend/mail.php
@@ -1,5 +1,6 @@
 
-
+<?php include 'header.php'; ?>
+<?php include 'sidebar.php'; ?>
 <!-- End Left Sidebar  -->
 <!-- Page wrapper  -->
 <div class="page-wrapper">
@@ -14,17 +15,16 @@
                 </ol>
             </div>
         </div>
-<?php
+                                                                                        <?php
 /**
  * This example shows how to handle a simple contact form.
  */
 
 //Import PHPMailer classes into the global namespace
 use PHPMailer\PHPMailer\PHPMailer;
-
+$msg = '';
 //Don't run this unless we're handling a form submission
 if (array_key_exists('email', $_POST)) {
-
     date_default_timezone_set('Etc/UTC');
     require 'autoload.php';
     //Create a new PHPMailer instance
@@ -44,53 +44,40 @@ $mail->Password = "cornellekacy456";
     //Use a fixed address in your own domain as the from address
     //**DO NOT** use the submitter's address here as it will be forgery
     //and will cause your messages to fail SPF checks
-    $mail->setFrom('from@example.com', 'FedExs USA');
+    $mail->setFrom('from@example.com', 'Site Contact');
     //Send the message to yourself, or whoever should receive contact for submissions
-    $mail->addAddress($_POST['email'], 'John Doe');
+    $mail->addAddress('contact@fedex-usa.net', 'Contact');
     //Put the submitter's address in a reply-to header
     //This will fail if the address provided is invalid,
     //in which case we should ignore the whole request
-    if ($mail->addReplyTo($_POST['email'], )) {
-        $mail->Subject = 'FedExs USA Logistics';
+    if ($mail->addReplyTo($_POST['email'], $_POST['fname'])) {
+        $mail->Subject = 'FedEx USA Contact';
         //Keep it simple - don't use HTML
-        $mail->isHTML(true);
+        $mail->isHTML(false);
         //Build a simple message body
-        $mail->AddEmbeddedImage('bar.png', 'logoimg', 'bar.png');
-        $mail->AddEmbeddedImage('pc.png', 'logoimg1', 'pc.png');
-            $jk = $_POST['jkname'];
-            $jkt = $_POST['tracking'];
-            $jktg = $_POST['gram'];
-        $mail->Body = "
-                 <img src=\"cid:logoimg1\" />
-                    <h3><strong style='color: rgb(255,153,0);'>HELLO</strong> <strong style='text-transform: capitalize; color: rgb(255,153,0);'> $jk </strong></h3>
-                    <p>Thank you for shipping with us. Your order $jktg of  a 'Discrete Package'  tracking number for your package  is provided along with the website below. </p>
-                    <br>
-                      <h3>Tracking No :  $jkt    </h3>  
-                    <img src=\"cid:logoimg\" />
-                    <br><br>
-                    <a href='www.fedex-usa.net/track.php'><button>Track Package Here</button></a>
-                    
-                    <br><br><br><br>
-                    <P style='font-size: 11px;'>This invoice  is processed by FedEx-USA Transports and logistics, Inc. If you need more information, please contact us at contact@fedex-usa.net</P>
-<P style='font-size: 11px;'>By using our  services, you agree to fedex-usa.net Privacy Notice and Conditions of Use.</P>
-<P style='font-size: 11px;'>This email was sent from a notification-only address that cannot accept incoming email. Please do not reply to this message</P>
-                        ";
+        $mail->Body = <<<EOT
+First Name: {$_POST['fname']}
+Last Name: {$_POST['lname']}
+Email: {$_POST['email']}
+Phone Number: {$_POST['phone']}
+Subject: {$_POST['subject']}
+Message: {$_POST['message']}
+EOT;
         //Send the message, check for errors
         if (!$mail->send()) {
             //The reason for failing to send will be in $mail->ErrorInfo
             //but you shouldn't display errors to users - process the error, log it on your server.
-            echo 'Sorry, something went wrong. Please try again later.'. $mail->ErrorInfo;
+            $msg = 'Sorry, something went wrong. Please try again later.'. $mail->ErrorInfo;
         } else {
-            echo '<script type="text/javascript">
-alert("Tracking number was successfully sent to jk");
-window.location.href = "mail.php";
-</script>';
+            echo "<div class='alert alert-success'>
+  <strong>Sent!</strong> Message successfully send, we will get back to you soon.
+</div>";
         }
     } else {
-        echo "Invalid email address, message ignored.";
+        $msg = 'Invalid email address, message ignored.';
     }
 }
-?>
+?> 
 
         <!-- End Bread crumb -->
         <!-- Container fluid  -->
